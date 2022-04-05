99f368b8d4e65be02afe5c6ae7da1316dcb28b59
docteurjay <aristejean@gmail.com>
2022-04-04 21:28:39 -0400

    Allowing service to customize the retry config

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -1,40 +1,53 @@
 using System;
 using System.Reflection;
 using GreenPipes;
+using GreenPipes.Configurators;
 using MassTransit;
 using MassTransit.Definition;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Play.Common.Settings;
 
 namespace Play.Common.MassTransit
 {
-    public static class Extensions 
+    public static class Extensions
     {
-        public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services) 
+        public static IServiceCollection AddMassTransitWithRabbitMQ(
+            this IServiceCollection services,
+            Action<IRetryConfigurator> configureRetries = null)
         {
-             services.AddMassTransit(configure => 
+            services.AddMassTransit(configure =>
             {
                 configure.AddConsumers(Assembly.GetEntryAssembly());
-
-                configure.UsingRabbitMq((context, configurator) => 
-                {
-                     var configuration = context.GetService<IConfiguration>();
-                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                    configurator.Host(rabbitMQSettings.Host);
-                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-                    configurator.UseMessageRetry(retryConfigurator => 
-                    {
-                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
-                    });
-                });
+                configure.UsingPlayEconomyRabbitMq(configureRetries);
             });
 
             // Start RabbitMQ Bus
             services.AddMassTransitHostedService();
 
             return services;
+        }
+
+        public static void UsingPlayEconomyRabbitMq(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
         }
     }
 }
