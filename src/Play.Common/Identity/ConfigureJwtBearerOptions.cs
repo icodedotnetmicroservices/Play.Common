4a76d581e4a2893e6d4097a29dbd47cc7548a080
docteurjay <aristejean@gmail.com>
2022-04-21 08:28:34 -0400

    Configure SignalR Authentication

--- src/Play.Common/Identity/ConfigureJwtBearerOptions.cs
+++ src/Play.Common/Identity/ConfigureJwtBearerOptions.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
@@ -8,6 +9,9 @@ namespace Play.Common.Identity
 {
     public class ConfigureJwtBearerOptions : IConfigureNamedOptions<JwtBearerOptions>
     {
+
+        private const string AccessTokenParameter = "access_token";
+        private const string MessageHubPath = "/messageHub";
         private readonly IConfiguration configuration;
 
         public ConfigureJwtBearerOptions(IConfiguration configuration)
@@ -30,6 +34,22 @@ namespace Play.Common.Identity
                 {
                     NameClaimType = "name",
                     RoleClaimType = "role"
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query[AccessTokenParameter];
+                        var path = context.HttpContext.Request.Path;
+
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments(MessageHubPath))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
                 };
             }
         }
