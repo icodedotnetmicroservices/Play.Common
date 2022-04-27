c54cbf926272955a39be44cd0c6f962efabcbfae
docteurjay <aristejean@gmail.com>
2022-04-27 08:31:59 -0400

    Added cosmodb support

--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -2,10 +2,16 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
         public string Host { get; init; }
         public int Port { get; init; }
 
         // Expression body definition, it's a property defined directly by the value on the right side.
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        // public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get { return string.IsNullOrWhiteSpace(connectionString) ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
+        }
     }
 }
